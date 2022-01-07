d7de441eff5f62c528714cba32bd3249f1fb7d2d
bryanmsombra27 <bryanjm96@gmail.com>
2022-01-06 23:07:21 -0600

    descargar el archivo xml de la url dada por el gobierno

--- controller/places.js
+++ controller/places.js
@@ -1,25 +1,36 @@
 const { response } = require("express");
 const path = require("path");
-
 const fs = require("fs");
+const https = require("https");
 const {XMLParser} = require("fast-xml-parser");
 
 const parser = new XMLParser();
 
 let lugares;
-fs.readFile(path.join(__dirname, "../data/places.xml"),'utf-8',(err,data)=>{
-    if(err) {
-        throw new Error('no se puede leer');
-    }
-    else{
-        // lugares =JSON.stringify(data);
-        lugares  = parser.parse(data); 
-    }
 
-
-
+//descargar archivo xml y almacenarlo en el server
+const file = fs.createWriteStream(path.join(__dirname, "../data/places.xml"));
+const request = https.get('https://publicacionexterna.azurewebsites.net/publicaciones/places',(response)=>{    
+    response.pipe(file)
+    const data = fs.readFileSync(path.join(__dirname, "../data/places.xml"),{encoding: "utf8"});
+    lugares  = parser.parse(data)
 })
 
+
+//carga de los lugares  lectura asyncrona
+// fs.readFile(path.join(__dirname, "../data/places.xml"),'utf-8',(err,data)=>{
+//     if(err) {
+//         throw new Error('no se puede leer');
+//     }
+//     else{
+//         // lugares =JSON.stringify(data);
+//         lugares  = parser.parse(data); 
+//     }
+// });
+
+//lectura sincrona
+// const data = fs.readFileSync(path.join(__dirname, "../data/places.xml"),{encoding: "utf8"});
+// lugares  = parser.parse(data)
 
 
 const places = (req,res = response) =>{
