f160e53df7721651d420f705a4dcf912386c48cc
bryanmsombra27 <bryanjm96@gmail.com>
2022-01-10 12:26:32 -0600

    Combinando data de prices y places

--- controller/places.js
+++ controller/places.js
@@ -28,7 +28,33 @@ const lugares = (req,res = response) =>{
     console.log('funcionando places')
     return res.status(200).send({status:'success',place,cantidad:place.length})
 }
+const lugaresPlaces = (req,res) =>{
+    const data = fs.readFileSync(path.join(__dirname, "../data/places.xml"),{encoding: "utf8"});
+    const data2 = fs.readFileSync(path.join(__dirname, "../data/prices.xml"),{encoding: "utf8"});
+    places  = parser.parse(data);
+    const precios  = parser.parse(data2);
+
+    //acapulco
+    const place = places.places.place.filter(place => place["@_place_id"] == 11415 || place["@_place_id"] == 12914 || place["@_place_id"] == 2798 || place["@_place_id"] == 13089 || place["@_place_id"] ==13090 || place["@_place_id"] ==13350 || place["@_place_id"] ==13411 || place["@_place_id"] == 3157 || place["@_place_id"] ==2869 ||place["@_place_id"] ==3014 || place["@_place_id"] ==2972 || place["@_place_id"] ==2773 ||place["@_place_id"]==2753 ||place["@_place_id"]==2876 ||place["@_place_id"]== 14079 ||place["@_place_id"]==14432 ||place["@_place_id"]==15659 ||place["@_place_id"]==15684 ||place["@_place_id"]== 16211 || place["@_place_id"]==20864 ||place["@_place_id"]==2669  || place["@_place_id"]==3305 || place["@_place_id"]== 21138 ||place["@_place_id"]==21639||place["@_place_id"]==21644 || place["@_place_id"]==21774|| place["@_place_id"]==22451|| place["@_place_id"]==24079|| place["@_place_id"]==25805|| place["@_place_id"]==26416|| place["@_place_id"]==26774|| place["@_place_id"]==3989|| place["@_place_id"]==10843|| place["@_place_id"]==11028|| place["@_place_id"]==6718|| place["@_place_id"]==7347|| place["@_place_id"]==6787|| place["@_place_id"]==6797|| place["@_place_id"]==6260|| place["@_place_id"]==6739|| place["@_place_id"]==6490|| place["@_place_id"]==7357|| place["@_place_id"]==6757|| place["@_place_id"]==6788|| place["@_place_id"]==6594|| place["@_place_id"]==6820|| place["@_place_id"]==6564|| place["@_place_id"]==6438|| place["@_place_id"]==6241|| place["@_place_id"]==6847|| place["@_place_id"]==6559|| place["@_place_id"]==7179|| place["@_place_id"]==6690|| place["@_place_id"]==6972|| place["@_place_id"]==5358|| place["@_place_id"]==5543|| place["@_place_id"]==5460|| place["@_place_id"]==7888|| place["@_place_id"]==8504|| place["@_place_id"]==8197|| place["@_place_id"]==8438|| place["@_place_id"]==8396|| place["@_place_id"]==8740|| place["@_place_id"]==8831|| place["@_place_id"]==9476|| place["@_place_id"]==9317|| place["@_place_id"]==9991|| place["@_place_id"]==9936|| place["@_place_id"]==10145|| place["@_place_id"]==10500|| place["@_place_id"]==10704)
+     const precio = precios.places.place.filter(precio => precio["@_place_id"] == 11415 || precio["@_place_id"] == 12914 || precio["@_place_id"] == 2798 || precio["@_place_id"] == 13089 || precio["@_place_id"] ==13090 || precio["@_place_id"] ==13350 || precio["@_place_id"] ==13411 || precio["@_place_id"] == 3157 || precio["@_place_id"] ==2869 ||precio["@_place_id"] ==3014 || precio["@_place_id"] ==2972 || precio["@_place_id"] ==2773 ||precio["@_place_id"]==2753 ||precio["@_place_id"]==2876 ||precio["@_place_id"]== 14079 ||precio["@_place_id"]==14432 ||precio["@_place_id"]==15659 ||precio["@_place_id"]==15684 ||precio["@_place_id"]== 16211 || precio["@_place_id"]==20864 ||precio["@_place_id"]==2669  || precio["@_place_id"]==3305 || precio["@_place_id"]== 21138 ||precio["@_place_id"]==21639||precio["@_place_id"]==21644 || precio["@_place_id"]==21774|| precio["@_place_id"]==22451|| precio["@_place_id"]==24079|| precio["@_place_id"]==25805|| precio["@_place_id"]==26416|| precio["@_place_id"]==26774|| precio["@_place_id"]==3989|| precio["@_place_id"]==10843|| precio["@_place_id"]==11028|| precio["@_place_id"]==6718|| precio["@_place_id"]==7347|| precio["@_place_id"]==6787|| precio["@_place_id"]==6797|| precio["@_place_id"]==6260|| precio["@_place_id"]==6739|| precio["@_place_id"]==6490|| precio["@_place_id"]==7357|| precio["@_place_id"]==6757|| precio["@_place_id"]==6788|| precio["@_place_id"]==6594|| precio["@_place_id"]==6820|| precio["@_place_id"]==6564|| precio["@_place_id"]==6438|| precio["@_place_id"]==6241|| precio["@_place_id"]==6847|| precio["@_place_id"]==6559|| precio["@_place_id"]==7179|| precio["@_place_id"]==6690|| precio["@_place_id"]==6972|| precio["@_place_id"]==5358|| precio["@_place_id"]==5543|| precio["@_place_id"]==5460|| precio["@_place_id"]==7888|| precio["@_place_id"]==8504|| precio["@_place_id"]==8197|| precio["@_place_id"]==8438|| precio["@_place_id"]==8396|| precio["@_place_id"]==8740|| precio["@_place_id"]==8831|| precio["@_place_id"]==9476|| precio["@_place_id"]==9317|| precio["@_place_id"]==9991|| precio["@_place_id"]==9936|| precio["@_place_id"]==10145|| precio["@_place_id"]==10500|| precio["@_place_id"]==10704)
+    
+    const precioyPlaces  = precio.filter(precio =>{
+      const lugargas =  place.find(lugar => lugar['@_place_id']  == precio["@_place_id"] );
+
+      precio.lugar = lugargas;
+
+      return precio
+    })
+
+    console.log(precioyPlaces);
+
+     return res.status(200).send({
+         status: 'success',
+         precios:precioyPlaces
+     }) 
+}
 
 module.exports = {
-    lugares
+    lugares,
+    lugaresPlaces
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const { prices,pricesById} = require('../controller/prices');
-const {lugares} = require('../controller/places')
+const {lugares,lugaresPlaces} = require('../controller/places')
 
 const router = require('express').Router();
 
@@ -7,5 +7,6 @@ const router = require('express').Router();
 router.get('/prices',prices);
 router.get('/prices/:id',pricesById);
 router.get('/places',lugares);
+router.get('/general',lugaresPlaces);
 
 module.exports = router;
