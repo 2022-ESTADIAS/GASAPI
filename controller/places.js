a50d0d986149877d2a66c0c904c3d022ed11d68e
bryanmsombra27 <bryanjm96@gmail.com>
2022-02-11 17:03:45 -0600

    ver registros iguales en una sola linea

--- controller/places.js
+++ controller/places.js
@@ -60,26 +60,26 @@ const ubicacionLugares = (req,res) =>{
       
         if(precio['gas_price'][0] && precio['gas_price'][1] && precio['gas_price'][2] ){
             precio.type = {
-                regular:{
+                [precio['gas_price'][0]['@_type']]:{
                     name: precio['gas_price'][0]['@_type'],
                     price:precio['gas_price'][0]['#text']
                 },
-                premium:{
+                [precio['gas_price'][1]['@_type']]:{
                     name: precio['gas_price'][1]['@_type'],
                     price:precio['gas_price'][1]['#text']
                 },
-                diesel:{
+                [precio['gas_price'][2]['@_type']]:{
                     name: precio['gas_price'][2]['@_type'],
                     price:precio['gas_price'][2]['#text']
                 },
             }
         }else if(precio['gas_price'][0] && precio['gas_price'][1]){
             precio.type = {
-                regular:{
+                [precio['gas_price'][0]['@_type']]:{
                     name: precio['gas_price'][0]['@_type'],
                     price:precio['gas_price'][0]['#text']
                 },
-                premium:{
+                [precio['gas_price'][1]['@_type']]:{
                     name: precio['gas_price'][1]['@_type'],
                     price:precio['gas_price'][1]['#text']
                 },
@@ -87,7 +87,7 @@ const ubicacionLugares = (req,res) =>{
         }else if(precio['gas_price'][0]){
             precio.type = {
               
-                regular:{
+                [ precio['gas_price'][0]['@_type']]:{
                     name: precio['gas_price'][0]['@_type'],
                     price:precio['gas_price'][0]['#text']
                 }
@@ -117,11 +117,151 @@ const ubicacionLugares = (req,res) =>{
 }
 
 
+const ubicacionLugaresUnSoloRegistro = (req,res) =>{
+    try{
+
+    const data = fs.readFileSync(path.join(__dirname, "../data/places.xml"),{encoding: "utf8"});
+    const data2 = fs.readFileSync(path.join(__dirname, "../data/prices.xml"),{encoding: "utf8"});
+    const lugaresXML  = parser.parse(data);
+    const preciosXML  = parser.parse(data2);
+
+    const lugar = arregloFiltradoPorLugarAcapulco(lugaresXML);
+    const precio = arregloFiltradoPorLugarAcapulco(preciosXML)
+
+    const precioyLugar  = precio.filter(precio =>{
+      const Gasolinera =  lugar.find(lugar => lugar['@_place_id']  == precio["@_place_id"] );
+      precio.lugar = Gasolinera;
+        
+      return precio
+    })
+    
+    const arregloDefinitivo =precioyLugar.filter(precio =>{
+      
+        if(precio['gas_price'][0] && precio['gas_price'][1] && precio['gas_price'][2] ){
+            precio.type = {
+                [precio['gas_price'][0]['@_type']]:{
+                    name: precio['gas_price'][0]['@_type'],
+                    price:precio['gas_price'][0]['#text']
+                },
+                [precio['gas_price'][1]['@_type']]:{
+                    name: precio['gas_price'][1]['@_type'],
+                    price:precio['gas_price'][1]['#text']
+                },
+                [precio['gas_price'][2]['@_type']]:{
+                    name: precio['gas_price'][2]['@_type'],
+                    price:precio['gas_price'][2]['#text']
+                },
+            }
+        }else if(precio['gas_price'][0] && precio['gas_price'][1]){
+            precio.type = {
+                [precio['gas_price'][0]['@_type']]:{
+                    name: precio['gas_price'][0]['@_type'],
+                    price:precio['gas_price'][0]['#text']
+                },
+                [precio['gas_price'][1]['@_type']]:{
+                    name: precio['gas_price'][1]['@_type'],
+                    price:precio['gas_price'][1]['#text']
+                },
+            }
+        }else if(precio['gas_price'][0]){
+            precio.type = {
+              
+                [ precio['gas_price'][0]['@_type']]:{
+                    name: precio['gas_price'][0]['@_type'],
+                    price:precio['gas_price'][0]['#text']
+                }
+
+            }  
+        }else{
+            precio.type ={
+                [ precio['gas_price']['@_type']]:{
+                    name: precio['gas_price']['@_type'],
+                    price:precio['gas_price']['#text']
+                } 
+           }
+        }
+
+        return precio
+    })
+
+    const arregloDefinitivoUnSoloRegistro =arregloDefinitivo.map((registro,i,arreglo)=>{
+        let regular = {};
+                             
+        // console.log(registro.type)
+        if(registro.lugar.cre_id.split("/")[1] == 9019 || registro.lugar.cre_id.split("/")[1] ==9016 ||
+        registro.lugar.cre_id.split("/")[1] == 9012 || registro.lugar.cre_id.split("/")[1] ==8229 ||
+        registro.lugar.cre_id.split("/")[1] == 7793 || registro.lugar.cre_id.split("/")[1] ==6832 ||
+        registro.lugar.cre_id.split("/")[1] == 4770 || registro.lugar.cre_id.split("/")[1] ==4765 ||
+        registro.lugar.cre_id.split("/")[1] == 4352 || registro.lugar.cre_id.split("/")[1] ==1087 ||
+        registro.lugar.cre_id.split("/")[1] == 4339 || registro.lugar.cre_id.split("/")[1] ==4332 ||
+        registro.lugar.cre_id.split("/")[1] == 4317 || registro.lugar.cre_id.split("/")[1] ==4289||
+        registro.lugar.cre_id.split("/")[1] == 4263 || registro.lugar.cre_id.split("/")[1] ==23041||
+        registro.lugar.cre_id.split("/")[1] == 20920 || registro.lugar.cre_id.split("/")[1] ==1206||
+        registro.lugar.cre_id.split("/")[1] == 1192 || registro.lugar.cre_id.split("/")[1] ==1190 ||
+        registro.lugar.cre_id.split("/")[1] == 1162 || registro.lugar.cre_id.split("/")[1] ==10951 ||
+        registro.lugar.cre_id.split("/")[1] == 10950 
+        ){
+            // console.log(registro['@_place_id'] == arreglo[i+1]['@_place_id'])
+            if(registro['@_place_id'] == arreglo[i+1]['@_place_id']){
+                if(registro.type.regular){
+                    // console.log(registro)
+                    // console.log(arreglo[i+1].type.premium)
+                     if(arreglo[i+1].type.premium && arreglo[i+1].type.diesel){
+                         registro.type.premium ={
+                            ...arreglo[i+1].type.premium,
+                           }
+                           registro.type.diesel = {
+                            ...arreglo[i+1].type.diesel,
+                           }
+                     }else if(arreglo[i+1].type.diesel){
+                        registro.type.diesel = {
+                            ...arreglo[i+1].type.diesel,
+                           }
+                     }else{
+                        registro.type.premium ={
+                            ...arreglo[i+1].type.premium,
+                           }
+                     }
+                    //  console.log(registro.type)
+                     console.log(registro)
+                }
+                // return registro
+            }
+
+            return registro
+        }else {
+            // return registro
+        }
+        // return registro
+
+    })
+
+
+    // console.log(arregloDefinitivoUnSoloRegistro[2])
+    
+
+
+     return res.status(200).send({
+         status: 'success',
+         precios:arregloDefinitivo
+     }) 
+
+
+    }catch(e){
+        return res.status(404).send({status:'error',msg: 'No se pudo leer el archivo'})
+
+    }
+
+}
+
+
+
 
 
 module.exports = {
     cantidadLugares,
-    ubicacionLugares
+    ubicacionLugares,
+    ubicacionLugaresUnSoloRegistro
 }
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const { Precios,preciosPorId} = require('../controller/prices');
-const {cantidadLugares,ubicacionLugares} = require('../controller/places')
+const {cantidadLugares,ubicacionLugares ,ubicacionLugaresUnSoloRegistro} = require('../controller/places')
 
 const router = require('express').Router();
 
@@ -8,5 +8,6 @@ router.get('/prices',Precios);
 router.get('/prices/:id',preciosPorId);
 router.get('/places',cantidadLugares);
 router.get('/general',ubicacionLugares);
+router.get('/definitivo',ubicacionLugaresUnSoloRegistro);
 
 module.exports = router;
